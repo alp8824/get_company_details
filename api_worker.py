c16bb20120322fde863d58b5cb981fb56a176aec
“Bogdan” <dbogdan@gmail.com>
2014-05-21 21:01:41 +0300

    Added fallback logic in case company not found by the name specified in the input file (modify company name format and finally perform a search by company name and try to match the required name). Added support for detecting and skipping empty lines in input file

--- api_worker.py
+++ api_worker.py
@@ -11,7 +11,7 @@ INPUT_CSV = 'input.csv'
 OUTPUT_CSV = 'output.csv'
 API_KEY = 'mq96jn265dfzs7bzzcnkdkdq'
 API_VERSION = 1
-NA = 'NA'
+NA = ' '
 
 def handle_error(function):
     """
@@ -51,25 +51,82 @@ def unicode_to_str(unicode_str):
     else:
         return unicode_str
 
+def check_details(details):
+    if not details:
+        return False
+    try:
+        aux = details['error']
+        return False
+    except:
+        return True
+
+def not_empty(lst):
+    for element in lst:
+        if element:
+            return True
+    return False
+
+def get_raw_details(cb, company_name):
+    print "Looking up {}...".format(company_name)
+    details = cb.company(company_name)
+    if not check_details(details):
+        cn = company_name.replace(' ','')
+        cn = cn.replace('.com','')
+        if cn != company_name:
+            print ("\t {}...".format(cn))
+            details = cb.company(cn)
+    if not check_details(details):
+        cn = company_name.lower().replace(' ','-')
+        cn = cn.replace('.','-')
+        if cn != company_name:
+            print ("\t {}...".format(cn))
+            details = cb.company(cn)
+    if not check_details(details):
+        return None
+    return details
+
 def get_company_details(cb, company_name):
     """
     Use company name to return a list of company details named in needed_details.
     """
     import time
     now = time.time()
-    details = cb.company(company_name.lower().strip().replace(' ','-'))
+
     details_list = []
     # 'Name'
+    company_name = company_name.strip()
+    new_company_name = company_name
     lappend(details_list, company_name)
+
+    # try and get company details
+    details = get_raw_details(cb, company_name)
     if not details:
+        print 'Searching for company {}...'.format(company_name)
+        search_details = cb.search(company_name)
+        if search_details['total'] == 0:
+            return details_list
+        else:
+            for result in search_details['results']:
+                try:
+                    if company_name in result['name'] \
+                            or result['name'] in company_name:
+                        new_company_name = result['name']
+                        break
+                except:
+                    pass
+        if new_company_name != company_name:
+            details = get_raw_details(cb, new_company_name)
+    if not details:
+        print "\t\tNOT FOUND"
         return details_list
+
     # 'Website'
     lappend(details_list, get_info(details, 'homepage_url'))
     # 'Status' 
     lappend(details_list, '')
     # 'PIC($m)'
     raised_amount = 0
-    investors = set()
+    investors_set = set()
     fund_rounds = get_info(details, 'funding_rounds')
     try:
         for funds in fund_rounds:
@@ -77,14 +134,17 @@ def get_company_details(cb, company_name):
             if amount: raised_amount += amount
             for investor in funds['investments']:
                 if investor['financial_org']:
-                    investors.add(investor['financial_org']['name'])
+                    investors_set.add(investor['financial_org']['name'])
     except:
         pass
-    if not investors: investors = NA
+    if not investors_set:
+        investors = NA
+    else:
+        investors = ','.join(investors_set)
     lappend(details_list, '$'+str(raised_amount))
     # 'Existing Investors'
     if raised_amount == 0: raised_amount = NA
-    lappend(details_list, ','.join(investors))
+    lappend(details_list, investors)
     # '# of Employees'
     lappend(details_list, get_info(details, 'number_of_employees'))
     # 'Alexa Traffic Global Rank'
@@ -132,9 +192,9 @@ def get_company_details(cb, company_name):
 @handle_error
 def main():
     cb = Crunchbase(API_KEY, API_VERSION)
-    from pprint import pprint
-    pprint(cb.company('fuelquest'))
-    exit()
+    # from pprint import pprint
+    # pprint(cb.company('Seven-Medical'))
+    # exit()
     with open(INPUT_CSV) as read_handler:
         with open (OUTPUT_CSV, 'w') as write_handler:
             reader = csv.reader(read_handler)
@@ -142,9 +202,10 @@ def main():
             headers = reader.next()
             writer.writerow(headers)
             for line in reader:
-                company_name = line[0]
-                new_line = get_company_details(cb, company_name)
-                writer.writerow(new_line)
+                if not_empty(line):
+                    company_name = line[0].strip()
+                    new_line = get_company_details(cb, company_name)
+                    writer.writerow(new_line)
     
 if __name__ == '__main__':
     main()
